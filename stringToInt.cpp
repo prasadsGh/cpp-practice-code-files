0fe514307e2d9c3574d1a71f9ba2652c983a3730
prasad patil <prasadpatil2309@gmail.com>
2022-06-22 11:31:13 +0530

    parse integer out of string

---
+++ stringToInt.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//parse integer out of string
+
+
+int main(){
+    string a="121";
+    int k=stoi(a);
+    cout<<k<<endl;
+return 0;
+}
+
+//output: 121
